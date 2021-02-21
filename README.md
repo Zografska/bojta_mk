32ca787bc3641bc9ce0a3f798abaca18e696df01
Zografska <aleksndra2@gmail.com>
2021-02-21 11:18:33 +0100

    register mail added

--- README.md
+++ README.md
@@ -4,10 +4,9 @@ Tasks:
 
 
 - koga ima product u shopping cart na user i adminot saka da go izbrise, se narusuva referential integrity. Treba da go razmislime toa.[jakov]
-- Prakjanje mail posle register - [jakov].
 - document download[j]
 - Prakjanje mail za order confirmation - [jakov].
-
+- contact forma da se premesti - [jakov]
 - da se olesni navigacijata niz stranata (da se dodadat back kopcinja itn..) - [togetherrr]
 - Povrzeme so postgre baza [togetherrrr]
 
--- src/main/java/com/example/bojta_mk/web/RegisterController.java
+++ src/main/java/com/example/bojta_mk/web/RegisterController.java
@@ -1,6 +1,9 @@
 package com.example.bojta_mk.web;
 
 import com.example.bojta_mk.model.enumerations.Role;
+import com.example.bojta_mk.model.exeptions.PasswordsDoNotMatchException;
+import com.example.bojta_mk.model.exeptions.UsernameAlreadyExistsException;
+import com.example.bojta_mk.service.MailService;
 import com.example.bojta_mk.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +16,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/register")
 public class RegisterController {
     private final UserService userService;
-
-    public RegisterController(UserService userService) {
+    private final MailService mailService;
+    public RegisterController(UserService userService, MailService mailService) {
         this.userService = userService;
+        this.mailService = mailService;
     }
 
     @GetMapping
-    public String getRegisterPage(Model model){
+    public String getRegisterPage(@RequestParam(required = false) String error, Model model){
+        if(error!=null) {
+            model.addAttribute("error", error);
+            model.addAttribute("hasError",  true);
+        }
+        else  model.addAttribute("hasError",  false);
+
         model.addAttribute("bodyContent", "register");
         return "master.html";
     }
@@ -30,7 +40,13 @@ public class RegisterController {
                                    @RequestParam String name,
                                    @RequestParam String surname,
                                    @RequestParam String phone){
-        userService.register(username,password,repeatedPassword,name,surname,phone, Role.ROLE_USER);
-        return "redirect:/home";
+        try {
+            userService.register(username, password, repeatedPassword, name, surname, phone, Role.ROLE_USER);
+            mailService.sendRegisterConfirmation(username, name, surname);
+            return "redirect:/home";
+        }catch (IllegalArgumentException | UsernameAlreadyExistsException | PasswordsDoNotMatchException e)
+        {
+            return "redirect:/register?error="+e.getMessage();
+        }
     }
 }
